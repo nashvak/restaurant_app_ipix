cd89cb1f855a3c57c8449679e289a80069a3b565
nashva <nashvakizhakkayil@gmail.com>
2024-03-22 23:22:17 +0530

    colors added

---
+++ lib/common/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class Appcolors {
+  Color backgroundColor = Colors.white;
+  Color appColor = Colors.orange;
+  Color ratingColor = Colors.green;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:restaurant_app_ipix/view/splash_screens/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,12 +13,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-       
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: ,
+      home: const SplashScreen(),
     );
   }
 }
-
