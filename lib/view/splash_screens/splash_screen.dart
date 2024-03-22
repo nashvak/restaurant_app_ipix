1c3693c0610eb0af679896451f9d7650de2b0256
nashva <nashvakizhakkayil@gmail.com>
2024-03-22 23:58:14 +0530

    splash screen added

--- lib/view/splash_screens/splash_screen.dart
+++ lib/view/splash_screens/splash_screen.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+import '../login_page/login_screen.dart';
+
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const LoginScreen(),
+        ),
+      );
+    });
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Image.asset(
+          'assets/images/food_app_logo.png',
+          width: 250,
+        ),
+      ),
+    );
   }
 }
